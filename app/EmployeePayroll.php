35365afa59d8c7423199e41f67b40ebd74a692b1
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-07-19 09:55:43 -0400

    Added worked days method to EmployePayroll

--- app/EmployeePayroll.php
+++ app/EmployeePayroll.php
@@ -7,7 +7,7 @@ use App\Helpers\Util;
 
 class EmployeePayroll
 {
-    function __construct($payroll, $procedure)
+    function __construct($payroll)
     {
         $contract = $payroll->contract;
         $employee = $contract->employee;
@@ -33,7 +33,8 @@ class EmployeePayroll
         $this->base_wage = $payroll->base_wage;
         $this->management_entity = $employee->management_entity->name;
         $this->management_entity_id = $employee->management_entity->id;
-        $this->worked_days = $payroll->worked_days;
+        $this->unworked_days = $payroll->unworked_days;
+        $this->worked_days = $this->workedDays($payroll);
 
         // Payable template
         $this->discount_old = null;
@@ -47,7 +48,7 @@ class EmployeePayroll
         $this->total_amount_discount_institution = null;
         $this->total_discounts = null;
         $this->payable_liquid = null;
-        $this->employeeDiscounts($payroll, $procedure);
+        $this->employeeDiscounts($payroll);
 
         // Employer template
         $this->contribution_insurance_company = null;
@@ -55,14 +56,14 @@ class EmployeePayroll
         $this->contribution_employer_solidary = null;
         $this->contribution_employer_housing = null;
         $this->total_contributions = null;
-        $this->employerContribution($procedure);
+        $this->employerContribution($payroll);
 
         // Extra data
         $this->position_group = $contract->position->position_group->name;
         $this->position_group_id = $contract->position->position_group->id;
         $this->employer_number = $contract->position->position_group->employer_number->number;
         $this->employer_number_id = $contract->position->position_group->employer_number->id;
-        $this->valid_contract = (is_null($contract->date_end) && $contract->status) ? true : (Carbon::parse($contract->date_end)->gte(Carbon::create($procedure->year, $procedure->month->id)->endOfMonth()) || Carbon::parse($contract->date_end)->gte(Carbon::create($procedure->year, $procedure->month->id, 30)) && $contract->status);
+        $this->valid_contract = (is_null($contract->date_end) && $contract->status) ? true : (Carbon::parse($contract->date_end)->gte(Carbon::create($payroll->procedure->year, $payroll->procedure->month->id)->endOfMonth()) || Carbon::parse($contract->date_end)->gte(Carbon::create($payroll->procedure->year, $payroll->procedure->month->id, 30)) && $contract->status);
     }
 
     public function setZeroAccounts() {
@@ -88,14 +89,14 @@ class EmployeePayroll
         $this->total_contributions = 0;
     }
 
-    private function employeeDiscounts($payroll, $procedure)
+    private function employeeDiscounts($payroll)
     {
         $this->quotable = $this->base_wage * $this->worked_days / 30;
-        $this->discount_old = Util::get_percentage($this->quotable, $procedure->discount_old);
-        $this->discount_common_risk = Util::get_percentage($this->quotable,$procedure->discount_common_risk);
-        $this->discount_commission = Util::get_percentage($this->quotable,$procedure->discount_commission);
-        $this->discount_solidary = Util::get_percentage($this->quotable, $procedure->discount_solidary);
-        $this->discount_national = Util::get_percentage($this->quotable, $procedure->discount_national);
+        $this->discount_old = Util::get_percentage($this->quotable, $payroll->procedure->discount_old);
+        $this->discount_common_risk = Util::get_percentage($this->quotable,$payroll->procedure->discount_common_risk);
+        $this->discount_commission = Util::get_percentage($this->quotable,$payroll->procedure->discount_commission);
+        $this->discount_solidary = Util::get_percentage($this->quotable, $payroll->procedure->discount_solidary);
+        $this->discount_national = Util::get_percentage($this->quotable, $payroll->procedure->discount_national);
         $this->total_amount_discount_law = $this->discount_old + $this->discount_common_risk + $this->discount_commission + $this->discount_solidary + $this->discount_national;
         $this->net_salary = $this->quotable - $this->total_amount_discount_law;
         $this->discount_rc_iva = $payroll->discount_rc_iva;
@@ -104,24 +105,70 @@ class EmployeePayroll
         $this->payable_liquid = round(($this->quotable - $this->total_discounts), 2);
     }
 
-    private function employerContribution($procedure)
+    private function employerContribution($payroll)
     {
-        $this->contribution_insurance_company = Util::get_percentage($this->quotable, $procedure->contribution_insurance_company);
-        $this->contribution_professional_risk = Util::get_percentage($this->quotable, $procedure->contribution_professional_risk);
-        $this->contribution_employer_solidary = Util::get_percentage($this->quotable, $procedure->contribution_employer_solidary);
-        $this->contribution_employer_housing = Util::get_percentage($this->quotable, $procedure->contribution_employer_housing);
+        $this->contribution_insurance_company = Util::get_percentage($this->quotable, $payroll->procedure->contribution_insurance_company);
+        $this->contribution_professional_risk = Util::get_percentage($this->quotable, $payroll->procedure->contribution_professional_risk);
+        $this->contribution_employer_solidary = Util::get_percentage($this->quotable, $payroll->procedure->contribution_employer_solidary);
+        $this->contribution_employer_housing = Util::get_percentage($this->quotable, $payroll->procedure->contribution_employer_housing);
         $this->total_contributions = round(($this->contribution_insurance_company + $this->contribution_professional_risk + $this->contribution_employer_solidary + $this->contribution_employer_housing), 2);
     }
 
-    public function setWorkedDays($worked_days, $payroll, $procedure)
+    public function setWorkedDays($worked_days, $payroll)
     {
         $this->worked_days = $worked_days;
-        $this->employeeDiscounts($payroll, $procedure);
-        $this->employerContribution($procedure);
+        $this->employeeDiscounts($payroll);
+        $this->employerContribution($payroll);
+    }
+
+    public function getWorkedDays()
+    {
+        return $this->worked_days;
     }
 
     public function setValidContact($valid_contract)
     {
         $this->valid_contract = $valid_contract;
+    }
+
+    private function workedDays($payroll)
+    {
+        $contract = $payroll->contract;
+
+        $payroll_date = (object)[
+            "year" =>  $payroll->procedure->year,
+            "month" => $payroll->procedure->month_id,
+        ];
+
+        $date_start = (object)[
+            "day" =>  Carbon::parse($contract->date_start)->day,
+            "year" =>  Carbon::parse($contract->date_start)->year,
+            "month" => Carbon::parse($contract->date_start)->month,
+        ];
+
+        $date_end = (object)[
+            "day" =>  Carbon::parse($contract->date_end)->day,
+            "year" =>  Carbon::parse($contract->date_end)->year,
+            "month" => Carbon::parse($contract->date_end)->month,
+        ];
+
+        $worked_days = 0;
+
+        if ($date_end == null) {
+            $worked_days = 30;
+        } elseif ($date_start->year <= $payroll_date->year && $date_start->month == $payroll_date->month) {
+            $worked_days = 30 + 1 - $date_start->day;
+        } elseif ($date_end->year >= $payroll_date->year && $date_end->month == $payroll_date->month) {
+            $worked_days = $date_end->day;
+        } elseif (($date_start->year <= $payroll_date->year && $date_start->month < $payroll_date->month) || ($date_end->year >= $payroll_date->year && $date_end->month > $payroll_date->month)) {
+            $worked_days = 30;
+        } else {
+            $worked_days = 0;
+        }
+        if ($payroll->unworked_days > $worked_days) {
+            return 0;
+        } else {
+            return ($worked_days - $payroll->unworked_days);
+        }
     }
 }
