45230878a41d0b869826c572e141e6b368529322
will <wian8887@gmail.com>
2018-08-02 12:26:02 -0400

    cambios

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -41,29 +41,16 @@ class ContractController extends Controller
     {
         $contracts = Contract::where('status',$status)->get();        
         foreach ($contracts as $key => $contract) {
-            $row = [];
-            $row[] = $contract->employee->identity_card;
-            $row[] = $contract->employee->last_name.' '.$contract->employee->mothers_last_name.' '.$contract->employee->first_name.' '.$contract->employee->second_name;
-            $row[] = $contract->position->name;
-            $row[] = $contract->position->position_group->name;
-            $row[] = date("d-m-Y", strtotime($contract->date_start));
-            if(!is_null($contract->date_end)) {
-                $row[] = date("d-m-Y", strtotime($contract->date_end));    
-            } else {
-                $row[] = '-';
-            }        
-            $row[] = $contract->employee->employee_type->name;
-            $row[] = ($contract->status == true?'<i class="fa fa-check"></i>':'<i class="fa fa-times"></i>');
+            
             if ( $contract->employee->employee_type_id == 3) {
-                $row[] = "
-                    <a class='btn btn-primary' type='button' href='contract/".$contract->id."'><i class='fa fa-eye'></i>&nbsp;Ver</a>
-                    <a class='btn btn-primary' type='button' href='contract/".$contract->id."/edit'><i class='fa fa-pencil'></i>&nbsp;Editar</a>
-                    <a class='btn btn-primary' type='button' href='contract/".$contract->id."/edit'><i class='fa fa-pencil'></i>&nbsp;Editar</a>
+                $options = "
+                    <a class='btn btn-primary' type='button' data-toggle='tooltip' title='Editar' href='contract/".$contract->id."/edit'><i class='fa fa-pencil'></i></a>
+                    <a class='btn btn-primary delete' type='button' data-toggle='tooltip' title='Eliminar' href='#' data='".$contract->id."'><i class='fa fa-close'></i></a>
                     ";
             } else {
-                $row[] = "
+                $options = "
                     <div class='btn-group' style=''>
-                        <button data-toggle='dropdown' class='btn btn-primary dropdown-toggle'>Imprimir <i class='fa fa-caret-down'></i></button>
+                        <button data-toggle='dropdown' class='btn btn-primary dropdown-toggle'><i class='fa fa-print'></i> <i class='fa fa-caret-down'></i></button>
                         <ul class='dropdown-menu bg-primary'>
                             <li>
                                 <a class='dropdown-item' 
@@ -79,12 +66,33 @@ class ContractController extends Controller
                             </li>
                         </ul>
                     </div>
-                    <a class='btn btn-primary' type='button' href='contract/".$contract->id."'><i class='fa fa-eye'></i>&nbsp;Ver</a>
-                    <a class='btn btn-primary' type='button' href='contract/".$contract->id."/edit'><i class='fa fa-pencil'></i>&nbsp;Editar</a>
-                    <a class='btn btn-primary delete' type='button' href='#' data='".$contract->id."'><i class='fa fa-close'></i>&nbsp;Eliminar</a>
+                    <a class='btn btn-primary' type='button' data-toggle='tooltip' title='Editar' href='contract/".$contract->id."/edit'><i class='fa fa-pencil'></i></a>
+                    <a class='btn btn-primary recontract' type='button' data-toggle='tooltip' title='Recontratar' href='contract/recontract/".$contract->id."'><i class='fa fa-refresh'></i></a>
+                    <a class='btn btn-primary delete' type='button' data-toggle='tooltip' title='Eliminar' href='#' data='".$contract->id."'><i class='fa fa-close'></i></a>
                     ";
             }
-            
+
+            $row = [];
+            $row[] = $key + 1;
+            $row[] = $options;
+            $row[] = $contract->employee->identity_card;
+            $row[] = $contract->employee->last_name.' '.$contract->employee->mothers_last_name.' '.$contract->employee->first_name.' '.$contract->employee->second_name;
+            $row[] = $contract->position->charge->name;
+            $row[] = $contract->position->name;
+            $row[] = $contract->position->position_group->name;
+            $row[] = $contract->ContractType->name;
+            $row[] = $contract->number_contract;
+            $row[] = date("d-m-Y", strtotime($contract->date_start));
+            $row[] = ($contract->date_end)? date("d-m-Y", strtotime($contract->date_end)):'-';
+            $row[] = ($contract->date_retirement)? date("d-m-Y", strtotime($contract->date_retirement)):'-';
+            $row[] = $contract->retirement_reason;
+            $row[] = $contract->number_insurance;
+            $row[] = $contract->cite_rrhh;
+            $row[] = $contract->cite_rrhh_date;
+            $row[] = $contract->numer_announcement;
+            $row[] = $contract->cite_performance;
+            $row[] = $contract->description;
+            $row[] = ($contract->status == true)? 'Vigente':'Concluido';
             $output['aaData'][] = $row;
         }        
         return response()->json($output);
@@ -295,7 +303,7 @@ class ContractController extends Controller
             Contract::find($id)->delete();
             return redirect('contract')->with('success', 'Eliminado correctamente');
         } else {
-            return redirect('contract')->with('error', 'No se pudo eliminar');            
+            return redirect('contract')->with('error', 'No se pudo eliminar, el contrato ya se encuentra en planillas');            
         }        
     }
     public function print(int $id)
@@ -415,5 +423,71 @@ class ContractController extends Controller
             $data[] = ['month'=> Util::getMonthEs($i),'day'=>$day,'salary'=>$salary];
         }
         return $data;
+    }
+    public function recontract ($id)
+    {
+        $contract = Contract::find($id);
+        $schedules = JobSchedule::all();
+        $employee = $contract->employee;
+        $position_groups = PositionGroup::get();
+        $position = Position::select('positions.id', 'positions.name', 'charges.base_wage')
+                                ->join('charges', 'positions.charge_id', '=', 'charges.id')
+                                ->get();
+        $schedules = JobSchedule::all();
+        $contract_type = ContractType::all();
+        $data = [
+            'contract' => $contract,
+            'employee' => $employee,
+            'employees' => Employee::where('status', true)->get(),
+            'position_groups' => $position_groups,
+            'position' => $position,
+            'schedules' => $schedules,
+            'contract_type' => $contract_type
+        ];
+        return view('contract.recontract',$data);
+    }
+    public function save_recontract(Request $request)
+    {
+        $messages = [
+            'required' => 'Campo obligatorio',
+        ];
+        $validator = Validator::make($request->all(), [
+            'date_start' => 'required',
+        ],$messages);
+
+        if ($validator->fails()) {
+            return redirect('contract/recontract/'.$request->id)
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+        $oldcontract = Contract::find($request->id);            
+        $oldcontract->status = false;
+        $oldcontract->retirement_reason = 'Conclusion de contrato';
+        $oldcontract->update();
+
+        $newcontract = new Contract();
+        $newcontract->employee_id = $oldcontract->employee_id;
+        $newcontract->position_id = $oldcontract->position_id;
+        $newcontract->date_start = $request->date_start;
+        $newcontract->date_end = $request->date_end;
+        $newcontract->number_insurance = $oldcontract->number_insurance;
+        $newcontract->contracts_type_id = 3;
+        $newcontract->status = true;
+        $newcontract->number_insurance = $request->number_insurance;
+        $newcontract->number_contract = $request->number_contract;
+        $newcontract->cite_rrhh = $request->cite_rrhh;
+        $newcontract->cite_rrhh_date = $request->cite_rrhh_date;
+        $newcontract->contracts_type_id = 3;
+        $newcontract->cite_performance = $request->cite_performance;
+        $newcontract->description = $request->description;
+        $newcontract->save();
+
+        $schedules = JobSchedule::all();
+        $sched_id = explode('|', $request->schedule);
+        $newcontract->schedules()->attach($schedules[$sched_id[0]-1]);
+        if (isset($sched_id[1])) {
+            $newcontract->schedules()->attach($schedules[$sched_id[1]-1]);
+        }
+        return redirect('contract')->with('success', 'Contrato renovado correctamente');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="{!! asset('css/sweetalert.css') !!}" />
     <link rel="stylesheet" href="{!! asset('css/datatables.min.css') !!}" />
     <link rel="stylesheet" href="{!! asset('css/datatables-fixedcolumns.min.css') !!}" />
+    <link rel="stylesheet" href="{!! asset('css/responsive.dataTables.min.css') !!}" />
     @section('styles')
     @show
 </head>
@@ -46,6 +47,7 @@
 <script src="{!! asset('js/app.js') !!}" type="text/javascript"></script>
 <script src="{!! asset('js/datatables.min.js') !!}" type="text/javascript"></script>
 <script src="{!! asset('js/datatables-fixedcolumns.min.js') !!}" type="text/javascript"></script>
+<script src="{!! asset('js/dataTables.responsive.js') !!}" type="text/javascript"></script>
 
 {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> --}}
 <script>
